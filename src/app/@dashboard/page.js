4e6ce788603873e7700492645d289073146bb17e
vasudesai <vasudesai1207@gmail.com>
2024-07-19 15:28:54 +0530

    heat map

--- src/app/@dashboard/page.js
+++ src/app/@dashboard/page.js
@@ -44,43 +44,74 @@ export default function Dashboard() {
   useEffect(() => {
     async function fetchIpAndInfo() {
       try {
-        // Fetch IP address
-        const ipResponse = await fetch('https://api.ipify.org?format=json');
+        const ipResponse = await fetch('https://ipapi.co/json');
         if (!ipResponse.ok) {
-          throw new Error(`Failed to fetch IP: ${ipResponse.statusText}`);
+          throw new Error('Network response was not ok');
         }
+
         const ipData = await ipResponse.json();
-        setIp(ipData.ip);
-        localStorage.setItem('userIp', ipData.ip);
 
-        // Fetch device info
-        const parser = new UAParser();
-        const deviceInfoString = `${parser.getDevice().type || 'Desktop'} - ${parser.getOS().name}`;
-        setDeviceInfo(deviceInfoString);
-        localStorage.setItem('deviceInfo', deviceInfoString);
+        // Create an object called details
+        const details = {
+          ip: ipData.ip,
+          network: ipData.network,
+          version: ipData.version,
+          city: ipData.city,
+          region: ipData.region,
+          region_code: ipData.region_code,
+          country: ipData.country,
+          country_name: ipData.country_name,
+          country_code: ipData.country_code,
+          country_code_iso3: ipData.country_code_iso3,
+          country_capital: ipData.country_capital,
+          country_tld: ipData.country_tld,
+          continent_code: ipData.continent_code,
+          in_eu: ipData.in_eu,
+          postal: ipData.postal,
+          longitude: ipData.longitude,
+          latitude: ipData.latitude,
+          timezone: ipData.timezone,
+          utc_offset: ipData.utc_offset,
+          country_calling_code: ipData.country_calling_code,
+          currency: ipData.currency,
+          currency_name: ipData.currency_name,
+          languages: ipData.languages,
+          country_area: ipData.country_area,
+          country_population: ipData.country_population,
+          asn: ipData.asn,
+          org: ipData.org
+        };
 
-        // Fetch location based on IP
-        const locationResponse = await fetch(`https://ipapi.co/${ipData.ip}/json/`);
-        if (!locationResponse.ok) {
-          throw new Error(`Failed to fetch location: ${locationResponse.statusText}`);
-        }
-        const locationData = await locationResponse.json();
-        const { latitude, longitude } = locationData;
-        setLocation({ lat: latitude, lon: longitude });
-        localStorage.setItem('userLocationCoordinate', JSON.stringify({ lat: latitude, lon: longitude }));
+        // Store the details object in localStorage as a JSON string
+        localStorage.setItem('details', JSON.stringify(details));
+        // // Fetch device info
+        // const parser = new UAParser();
+        // const deviceInfoString = `${parser.getDevice().type || 'Desktop'} - ${parser.getOS().name}`;
+        // setDeviceInfo(deviceInfoString);
+        // localStorage.setItem('deviceInfo', deviceInfoString);
 
-        // Reverse geocoding to get address
-        const addressResponse = await fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`);
-        if (!addressResponse.ok) {
-          throw new Error(`Failed to fetch address: ${addressResponse.statusText}`);
-        }
-        const addressData = await addressResponse.json();
-        const { country, state, city } = addressData.address;
-        setAddress({ country, state, city });
+        // // Fetch location based on IP
+        // const locationResponse = await fetch(`https://ipapi.co/${ipData.ip}/json/`);
+        // if (!locationResponse.ok) {
+        //   throw new Error(`Failed to fetch location: ${locationResponse.statusText}`);
+        // }
+        // const locationData = await locationResponse.json();
+        // const { latitude, longitude } = locationData;
+        // setLocation({ lat: latitude, lon: longitude });
+        // localStorage.setItem('userLocationCoordinate', JSON.stringify({ lat: latitude, lon: longitude }));
 
-        // Store combined address in local storage
-        const addressString = `${city || 'N/A'}, ${state || 'N/A'}, ${country || 'N/A'}`;
-        localStorage.setItem('address', addressString);
+        // // Reverse geocoding to get address
+        // const addressResponse = await fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`);
+        // if (!addressResponse.ok) {
+        //   throw new Error(`Failed to fetch address: ${addressResponse.statusText}`);
+        // }
+        // const addressData = await addressResponse.json();
+        // const { country, state, city } = addressData.address;
+        // setAddress({ country, state, city });
+
+        // // Store combined address in local storage
+        // const addressString = `${city || 'N/A'}, ${state || 'N/A'}, ${country || 'N/A'}`;
+        // localStorage.setItem('address', addressString);
 
       } catch (error) {
         console.error('Error fetching the IP address or information:', error);
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Script from 'next/script';
+import Hotjar from '@hotjar/browser';
 
 export default function Home() {
+  const siteId = 5060817;
+  const hotjarVersion = 6;
+  Hotjar.init(siteId, hotjarVersion);
   return (
-    <div>
-
-    </div >
+    <>
+      <Script
+        id="vtag-ai-js"
+        src="https://r2.leadsy.ai/tag.js"
+        strategy="lazyOnload"
+        data-pid="wfpwuAhJsLaYkTQo"
+        data-version="062024"
+      />
+    </>
   );
 }
